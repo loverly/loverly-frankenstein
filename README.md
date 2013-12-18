4e71ad889aae16a3cc7272da16b6341a8cb81aac
Brandon Eum <brandon@lover.ly>
2013-12-17 19:37:06 -0500

    Initial migration of files from existing structure

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-loverly-frankenstein
-====================
+# loverly-frankenstein
 
-The loverly node-js model layer that allows a unified hierarchical structure on top of an assortment of data-storage engines
+
+The loverly node-js model layer that allows a unified hierarchical structure on 
+top of an assortment of data-storage engines.  Want a nice JSON-based document 
+on top of a MySQL data store? No problem! __loverly-frankenstein__ is here to 
+save the day.
+
+The goal of this module is to abstract the underlying data storage engines from 
+the end-user's interactions, creating a clean easy-to-use presentation layer that 
+can fit into a standardized REST API.
+
+# Usage
+
+## Setting up loverly-frankenstein
+
+What ORM should I use?
+
+## Create a new model from MySQL
+
+### Data types
+
+### Aliases
+
+### One-to-one relationships
+
+### One-to-many relationships
+
+### Many-to-many relationships
+
+
+## Create a new model from an API
+
+
+## Setting up the CRUD routes
+
+# Examples
+
+# Run the Tests
---
+++ index.js
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ *
+ * index.js 
+ *
+ * Date:   December 2013
+ * Author: Brandon Eum <brandon@lover.ly>
+ *
+ ******************************************************************************/	
+
+/**
+ * Allow access to all of the base classes through the loverly frankenstein
+ * object returned when requiring this file.
+ */
+module.exports = (function () {
+  var exports = {
+    Model: require('./lib/AbstractModel.js'),
+    Instance: require('./lib/Instance.js'),
+    Table: require('./lib/AbstractTable.js'),
+    API: require('./lib/AbstractApi.js'),
+    File: require('./lib/AbstractFile.js'),
+    Validator: require('./lib/AbstractValidator.js'),
+    Orm: require('./lib/Orm.js')
+  };
+
+  // A set of initialized ORM connections
+  var connections = {};
+
+
+  // TODO: Think of a way to provide convenience methods to make it easier to use
+  //       this library
+
+  exports.createOrmConnection = function () {};
+  exports.createModel = function () {};
+
+  return exports;
+}());
