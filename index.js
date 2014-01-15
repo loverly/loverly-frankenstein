aab0e94abe4212cce017de8316799955245e71de
Brandon Eum <brandon@lover.ly>
2014-01-15 12:07:15 -0500

    Added an AbstractApiClient class to provide a uniform API wrapper interface

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /*******************************************************************************
  *
- * index.js 
+ * index.js
  *
  * Date:   December 2013
  * Author: Brandon Eum <brandon@lover.ly>
  *
- ******************************************************************************/	
+ ******************************************************************************/
 
 /**
  * Allow access to all of the base classes through the loverly frankenstein
@@ -13,19 +13,16 @@
  */
 module.exports = (function () {
   var exports = {
+    Source: require('./lib/AbstractSource.js'),
     Model: require('./lib/AbstractModel.js'),
     Instance: require('./lib/Instance.js'),
     Table: require('./lib/AbstractTable.js'),
     ManyToManyTable: require('./lib/AbstractManyToManyTable.js'),
-    //API: require('./lib/AbstractApi.js'),
+    ApiClient: require('./lib/AbstractApiClient.js'),
     File: require('./lib/AbstractFile.js'),
     Validator: require('./lib/AbstractValidator.js'),
     Orm: require('./lib/Orm.js')
   };
-
-  // A set of initialized ORM connections
-  var connections = {};
-
 
   // TODO: Think of a way to provide convenience methods to make it easier to use
   //       this library
